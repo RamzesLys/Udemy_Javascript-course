829bbc5280460bcce6312db24f00be546ae7e9b1
RamzesLys <ramzes311982@gmail.com>
2023-07-02 14:08:36 +0300

    Lesson 11 DataTypes +

---
+++ Lesson 11  Variables/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let number = 4.6;
+
+
+const persone = `Alex`;
+
+const bool = false;
+
+let und;
+
+
+const obj = {
+  name: 'Roman',
+  age: 41,
+  isMarried: false
+};
+
+
+// console.log(obj['age']);
+
+let arr =  ['plum.png', 'orange.jpeg', 6, 'apple.bmp', {}];
+console.log(arr[1]);
