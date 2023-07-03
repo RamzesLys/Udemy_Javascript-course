59c643437af53955060aa05f8753962601ba6004
RamzesLys <ramzes311982@gmail.com>
2023-07-03 22:47:43 +0300

    Lesson 13 Interaction with the user

---
+++ Lesson 13 Interaction with the user/js/script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// alert('Hello')
+
+// const result = confirm("Привіт!");
+// console.log(result);
+
+// const answer = +prompt("Скільки Вам років", "18");
+
+// console.log((answer + 5));
+
+const answers = [];
+answers[0] = prompt('Як тебе звати?', ' ');
+answers[1] = prompt('Як твоє прізвище?', '');
+answers[2] = prompt('Скільки тобі років?', '');
+
+console.log(typeof(answers))
