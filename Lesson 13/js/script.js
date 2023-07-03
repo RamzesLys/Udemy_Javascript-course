cd35988907b02e30b2e7bcd814764c32329804d0
RamzesLys <ramzes311982@gmail.com>
2023-07-03 13:19:36 +0300

    Lesson 13 Interaction 06:00

---
+++ Lesson 13/js/script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+// alert('Hello')
+
+// const result = confirm("Привіт!");
+// console.log(result);
+
+const answer = prompt("Скільки Вам років", "18");
+  if (answer < 18 || answer > 40) {
+    alert('Вхід заборонеко')
+  }
+console.log(answer);
