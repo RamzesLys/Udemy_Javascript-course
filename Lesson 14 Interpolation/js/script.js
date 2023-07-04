4fd569176fbd387ca5957df2d1b0c200145f53d1
RamzesLys <ramzes311982@gmail.com>
2023-07-04 13:17:31 +0300

    Lesson 15 Operators +

--- Lesson 14 Interpolation/js/script.js
+++ Lesson 14 Interpolation/js/script.js
@@ -2,7 +2,9 @@
 
 const category = 'toys';
 
+//OLD Method
 // console.log('https://someurl.com/' + category + '/' + '4');
 
+//NEW Method
 console.log(`https://someurl.com/${category}/5`);
 
--- Lesson 15 Operators/js/script.js
+++ Lesson 15 Operators/js/script.js
@@ -1,8 +1,28 @@
 "use strict"
 
-const category = 'toys';
+console.log('arr' + " - object");
+console.log(4 + +"5");
 
-// console.log('https://someurl.com/' + category + '/' + '4');
+let incr = 10;
+let decr = 10;
 
-console.log(`https://someurl.com/${category}/5`);
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(decr--);
+
+// console.log(5%2);
+
+console.log(2+2*2 !== '6');
+
+//&& - оператор "і, та"
+
+//|| -  оператор "або"
+
+const isChecked = true;
+      isClose = false;
+
+// console.log(isChecked && isClose);
+console.log(isChecked || !isClose);
 
