78b415e5dc398291df10de526f7c448473714f02
Roman <ramzes311982@gmail.com>
2023-07-18 19:58:05 +0300

    Lesson 36 ООП +

--- Lesson 36 OОП/js/script.js
+++ Lesson 36 OОП/js/script.js
@@ -7,3 +7,29 @@ console.log(typeof(str));
 console.log(typeof(strObj));
 
 console.dir([1, 2, 3]);
+
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: () => {
+    console.log('Hello');
+  }
+};
+
+//новий метод 2
+const john = Object.create(soldier)
+
+
+// const john = {
+//   health: 100
+// };
+
+//старий метод
+// john.__proto__ = soldier;
+
+//новий метод 
+Object.setPrototypeOf(john, soldier);
+
+
+// console.log(john.armor);
+john.sayHello();
