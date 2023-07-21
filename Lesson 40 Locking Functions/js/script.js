16b7afb525045ec51dc04875598f827ed7e1f338
Roman <ramzes311982@gmail.com>
2023-07-21 19:07:39 +0300

    Lesson 40 time 07:50

--- Lesson 40 Locking Functions/js/script.js
+++ Lesson 40 Locking Functions/js/script.js
@@ -1,12 +1,13 @@
 "use strict"
 
-let number = 5;
+let number = 5;debugger
 
 function logNumber() {
+  let number = 4;debugger
   console.log(number);
 }
 
 number = 6;
 
-logNumber();
+logNumber();debugger
 
