6c89ad91a11b495e4a32380be10d4e548548fb7d
Roman <ramzes311982@gmail.com>
2023-07-22 12:16:25 +0300

    Lesson 40 +

--- Lesson 40 Locking Functions/js/script.js
+++ Lesson 40 Locking Functions/js/script.js
@@ -1,13 +1,48 @@
 "use strict"
 
-let number = 5;debugger
+// let number = 5;debugger
 
-function logNumber() {
-  let number = 4;debugger
-  console.log(number);
+// function logNumber() {
+//   console.log(number);debugger
+// }
+
+// number = 6;
+
+// logNumber();debugger
+
+
+// number = 8;
+
+// logNumber();debugger
+
+function  createCounter () {
+  let counter = 0;
+
+  const myFunction = function () {debugger
+    counter = counter + 1;debugger
+    return counter;debugger
+  }
+  return myFunction;
 }
+debugger
 
-number = 6;
+const increment = createCounter();debugger
+const c1 = increment();debugger
+const c2 = increment();debugger
+const c3 = increment();debugger
 
-logNumber();debugger
 
+console.log(c1, c2, c3);
+
+//{
+//   let msg = 'Hello'
+// }
+// console.log(msg);
+
+// for (let i = 0; i < 9; i ++) {
+//   for(let j = 0; j < 9; j ++) {
+//     let num = 3;
+//     console.log(num);
+//   }
+
+// }
