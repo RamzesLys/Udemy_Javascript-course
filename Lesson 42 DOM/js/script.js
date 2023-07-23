f14cb3e3cf1286f71e319197b6d8b2da0793fad5
Roman <ramzes311982@gmail.com>
2023-07-23 18:26:16 +0300

    Lesson 42 +

---
+++ Lesson 42 DOM/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const box = document.getElementById('box');
+console.log(box);
+
+const btns = document.getElementsByTagName('button');
+console.log(btns);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+console.log(hearts);
+
+hearts.forEach(item => {
+  console.log(item);
+})
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
