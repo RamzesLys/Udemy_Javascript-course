7c86ab1b0c906262949948ef0d492236a01dccda
Roman <ramzes311982@gmail.com>
2023-07-28 18:36:22 +0300

    Lesson 45 -

---
+++ Lesson 46 DOM navigation/js/script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('#current').parentNode.parentNode);
+
+// console.log(document.body.firstElementChild);
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+// console.log(document.querySelector('#current').parentNode.parentElement); // на відміну від parentNode передає елемент а ну вузол 
+
+for(let node of document.body.childNodes) {
+  console.log(node);
+}
