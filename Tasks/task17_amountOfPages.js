906a7590a8c02482699c6c82919e4517455c61fd
Roman <ramzes311982@gmail.com>
2023-08-13 10:11:59 +0300

    Task 17 +

---
+++ Tasks/task17_amountOfPages.js
@@ -0,0 +1,17 @@
+function amountOfPages(summary){
+    let result = '';
+    let n = 0;
+
+    for (let i = 1; i <= summary; i++) {
+      result += i;
+      if (result.length === summary) {
+        n = i;
+        break;
+      }
+    }
+
+    return n;
+  }
+
+
+  console.log(amountOfPages(318));
