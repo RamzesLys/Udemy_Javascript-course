cc1b2a43cd28c291204bb668c1a0f8406327cbad
Roman <ramzes311982@gmail.com>
2023-08-31 15:40:00 +0300

    Test 3 +

---
+++ Tasks/test3.js
@@ -0,0 +1,38 @@
+//1
+function setOptions(height, width, ...additional) {
+  console.log(height, width, ...additional)
+}
+setOptions(400, 500, 'red', 'top');
+
+//2
+function getSum(a, b) {
+  function sum() {
+      console.log(this.a);
+      return a + b;
+  }
+
+  console.log(sum());
+}
+
+getSum(4, 5);
+
+//3
+const urlObj = {
+  protocol: 'https',
+  domain: 'mysite.com'
+}
+
+function showCurrentURL() {
+  const extractCurrDomain = () => {
+      return this.domain;
+  }
+  const extractCurrProtocol = () => {
+      return this.protocol;
+  }
+
+  console.log(`${extractCurrProtocol()}://${extractCurrDomain()}`)
+}
+
+const url = showCurrentURL.bind(urlObj);
+
+console.log(url);
