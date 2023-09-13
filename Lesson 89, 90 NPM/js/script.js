6115d9857aef4989f32bbb3016dddbd2ebdda0a5
Roman <ramzes311982@gmail.com>
2023-09-13 19:05:04 +0300

    Lesson 91 +

--- Lesson 89, 90 NPM/js/script.js
+++ Lesson 89, 90 NPM/js/script.js
@@ -215,35 +215,45 @@ window.addEventListener('DOMContentLoaded', function() {
     return await res.json();
   };
 
-  // getResource('http://localhost:3000/menu')
-  //     .then(data => {
-  //       data.forEach(({img, altimg, title, descr, price}) => {
-  //         new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-  //       });
-  //     });
 
-  //Ще один спосіб (без використання класів)
+  //Cпосіб 1 з класами
   getResource('http://localhost:3000/menu')
-      .then(data => createCard(data));
+      .then(data => {
+        data.forEach(({img, altimg, title, descr, price}) => {
+          new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        });
+      });
 
-  function createCard(data ) {
-    data.forEach(({img, altimg, title, descr, price}) => {
-      const element = document.createElement('div');
-      price = Math.round(price * 36,6);
-      element.classList.add('menu__item');
-      element.innerHTML = `
-      <img src=${img} alt=${altimg} >
-      <h3 class="menu__item-subtitle">${title}</h3>
-      <div class="menu__item-descr">${descr}</div>
-      <div class="menu__item-divider"></div>
-      <div class="menu__item-price">
-        <div class="menu__item-cost">Цена:</div>
-        <div class="menu__item-total"><span>${price}</span> грн/день</div>
-      </div>
-      `;
-      document.querySelector('.menu .container').append(element)
-    });
-  } 
+  //Cпосіб 2 (без використання класів)
+  // getResource('http://localhost:3000/menu')
+  //     .then(data => createCard(data));
+
+  // function createCard(data ) {
+  //   data.forEach(({img, altimg, title, descr, price}) => {
+  //     const element = document.createElement('div');
+  //     price = Math.round(price * 36,6);
+  //     element.classList.add('menu__item');
+  //     element.innerHTML = `
+  //     <img src=${img} alt=${altimg} >
+  //     <h3 class="menu__item-subtitle">${title}</h3>
+  //     <div class="menu__item-descr">${descr}</div>
+  //     <div class="menu__item-divider"></div>
+  //     <div class="menu__item-price">
+  //       <div class="menu__item-cost">Цена:</div>
+  //       <div class="menu__item-total"><span>${price}</span> грн/день</div>
+  //     </div>
+  //     `;
+  //     document.querySelector('.menu .container').append(element)
+  //   });
+  // } 
+
+  //Спосіб 3 через бібліотеку axios
+  // axios.get('http://localhost:3000/menu')
+  //       .then(data => {
+  //         data.data.forEach(({img, altimg, title, descr, price}) => {
+  //       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //        });
+  //     });
 
   //один з варіантів
   //const div = new MenuCard();
