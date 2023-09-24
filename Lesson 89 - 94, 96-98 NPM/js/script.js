d925b72cf08a6e584abbe3d918228a7c3ef86a2a
Roman <ramzes311982@gmail.com>
2023-09-24 15:57:22 +0300

    rename

--- Lesson 89 - 94, 96-98 NPM/js/script.js
+++ Lesson 89 - 94, 96-98 NPM/js/script.js
@@ -634,11 +634,28 @@ window.addEventListener('DOMContentLoaded', function() {
   //CALCULATOR
 
   const result = document.querySelector('.calculating__result span');
-  let sex = 'female', 
+
+  let sex, 
   height, 
   weight, 
   age, 
-  ratio = 1.375;
+  ratio;
+
+  //отримуємо значення з Local Storage в разі його наявності
+  if(localStorage.getItem('sex')) {
+    sex = localStorage.getItem('sex');
+  } else {
+    sex = 'female';
+    localStorage.setItem('sex', 'female'); // якщо немає то встановлюємо у значення female та записуємо у Local Storage
+  }
+
+  //те-ж саме з ratio
+  if(localStorage.getItem('ratio')) {
+   ratio = localStorage.getItem('ratio');
+  } else {
+    ratio = 1.375;
+    localStorage.setItem('ratio', 1.375); // якщо немає то встановлюємо у значення 1.375 та записуємо у Local Storage
+  }
 
   function calcTotal() {
     if (!sex || !height || !weight, !age || !ratio) {
@@ -655,17 +672,40 @@ window.addEventListener('DOMContentLoaded', function() {
 
   calcTotal();
 
-  function getStaticInformation (parentSelector, activeClass) {
-    const element = document.querySelectorAll(`${parentSelector} div`);
+  function initLocalSettings(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
 
-    element.forEach(elem => {
+    elements.forEach(elem => {
+      elem.classList.remove(activeClass);
+      if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+        elem.classList.add(activeClass);
+      }
+      if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+        elem.classList.add(activeClass)
+      }
+    });
+  }
+
+  initLocalSettings('#gender div', 'calculating__choose-item_active');
+  initLocalSettings('.calculating__choose_big', 'calculating__choose-item_active');
+
+  
+
+  function getStaticInformation (selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach(elem => {
       elem.addEventListener('click', (e) => {
         if (e.target.getAttribute('data-ratio')) {
           ratio = +e.target.getAttribute('data-ratio');
+          // записуємо коефіцієнт активності у LocaL Storage
+          localStorage.setItem('ratio', +e.target.getAttribute('data-ratio')); 
         } else {
-          sex = e.target.getAttribute('id')
+          sex = e.target.getAttribute('id');
+           // записуємо вибрану стать у LocaL Storage
+           localStorage.setItem('sex', e.target.getAttribute('id'));
         }
-        element.forEach( elem => {
+        elements.forEach( elem => {
           elem.classList.remove(activeClass);
         });
         e.target.classList.add(activeClass);
@@ -675,12 +715,20 @@ window.addEventListener('DOMContentLoaded', function() {
     })
   }
 
-  getStaticInformation('#gender', 'calculating__choose-item_active');
+  getStaticInformation('#gender div', 'calculating__choose-item_active');
   getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
 
   function getDinamicInformation (selector) {
     const input = document.querySelector(selector);
     input.addEventListener('input', () => {
+
+      //використовуємо регулярні вирази для перевірки значень, введених в input
+    if (input.value.match(/\D/g)) {
+      input.style.border = '1px solid red';
+    } else {
+      input.style.border = 'none';
+    }
+
       switch (input.getAttribute('id')) {
         case 'height': 
           height = +input.value;
