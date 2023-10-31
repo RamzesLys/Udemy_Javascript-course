ffff2680e7a2564b4f0786367f8cbf91c019cf33
Roman <ramzes311982@gmail.com>
2023-10-31 18:59:07 +0200

    Lesson 126 37:50 minutes

---
+++ Lesson 126 new Project React JS/employees/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css'
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Облік працівників компанії Imbirka</h1>
+      <h2>Загальна кількість працівників</h2>
+      <h2>Преміювання: </h2>
+    </div>
+  )
+}
+
+export default AppInfo;
---
+++ Lesson 126 new Project React JS/employees/src/components/app/app.js
@@ -0,0 +1,23 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <AppInfo/>
+
+      <div className="search-panel">
+        <SearchPanel/>
+        <AppFilter/>
+       
+      </div>
+    <EmployeesList/>
+    </div>
+  )
+}
+
+export default App;
