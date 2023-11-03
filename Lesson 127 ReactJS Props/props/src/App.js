33c6b1581b591b6903f28b77d51c8f9f2fa49c35
RamzesLys <ramzes311982@gmail.com>
2023-11-03 13:41:28 +0200

    Lesson 127 +

--- Lesson 127 ReactJS Props/props/src/App.js
+++ Lesson 127 ReactJS Props/props/src/App.js
@@ -2,23 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function WhoAmI (props) {
+// function WhoAmI (props) {
+//   return (
+//     <div>
+//       <h1>My name is {props.name}, surname - {props.surname}</h1>
+//       <a href={props.link}>My profile</a>
+//     </div>
+//   )
+// }
+
+//або з диструктуризацією
+function WhoAmI({name, surname, link}) {
   return (
     <div>
-      <h1>My name is {props.name}, surname - {props.surname}</h1>
-      <a href={props.link}>My profile</a>
+      <h1>My name is {name()}, surname - {surname}</h1>
+      <a href={link}>My profile</a>
     </div>
   )
 }
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <WhoAmI name={{firstName: 'John'}} surname="Smith" link="https://facebook.com"/>
+//       <WhoAmI name={{firstName: 'Alex'}} surname="Dovbeshko" link="https://www.instagram.com/"/>
+//     </div>
+//   );
+// }
+
+
+//пропсами також можна передавати не тільки об'кти, а й функції, які слід передавати з викликом
 
 function App() {
   return (
     <div className="App">
-      <WhoAmI name="John" surname="Smith" link="https://facebook.com"/>
-      <WhoAmI name="John" surname="Smith" link="https://facebook.com"/>
+      <WhoAmI name={() => { return 'John'}} surname="Smith" link="https://facebook.com"/>
+      <WhoAmI name={() => { return 'Vasiliy'}} surname="Dovbeshko" link="https://www.instagram.com/"/>
     </div>
   );
 }
-
 export default App;
