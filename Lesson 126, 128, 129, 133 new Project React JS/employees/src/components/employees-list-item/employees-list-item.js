9261501b92668ccab0e5952ae064bb7fc8ff5278
RamzesLys <ramzes311982@gmail.com>
2023-11-10 14:41:29 +0200

    Lesson 133 + HomeTask

---
+++ Lesson 126, 128, 129, 133 new Project React JS/employees/src/components/employees-list-item/employees-list-item.js
@@ -0,0 +1,63 @@
+import { Component } from 'react';
+import './employees-list-item.css';
+
+
+class EmployeesListItem extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    increase: false,
+    mark: false
+  }
+}
+
+onIncrease = () => {
+  this.setState(({increase}) => ({
+    increase: !increase
+  }))
+}
+
+onMark = () => {
+  this.setState(({mark}) => ({
+    mark: !mark
+  }))
+}
+
+
+
+  render() {
+    const {name, salary} = this.props;
+    const {increase, mark} = this.state
+
+    let classNames = "list-group-item d-flex justify-content-between";
+    if (increase) {
+    classNames += ' increase'
+  }
+
+  if (mark) {
+    classNames += ' like'
+  }
+
+  return (
+    <li className={classNames}>
+      <span onClick={this.onMark}className="list-group-item-label">{name}</span>
+      <input type="text" className="list-group-item-input" defaultValue={salary + ' $'} />
+      <div className="d-flex justify-content-center align-items-center">
+        <button type="button"
+        className="btn-cookie btn-sm"
+        onClick={this.onIncrease}>
+          <i className="fas fa-cookie"></i>
+        </button>
+        
+        <button type="button"
+        className="btn-trash btn-sm">
+          <i className="fas fa-trash"></i>
+        </button>
+          <i className="fa-regular fa-star"></i>
+      </div>
+    </li>
+  )
+  }
+}
+
+export default EmployeesListItem;
