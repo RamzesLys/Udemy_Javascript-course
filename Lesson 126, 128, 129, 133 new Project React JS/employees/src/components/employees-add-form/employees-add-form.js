dfc5f3fc8a2ceaf42e9bbd1003710af3d3d06950
RamzesLys <ramzes311982@gmail.com>
2023-11-13 14:16:57 +0200

    Lesson 134 +

--- Lesson 126, 128, 129, 133 new Project React JS/employees/src/components/employees-add-form/employees-add-form.js
+++ Lesson 126, 128, 129, 133 new Project React JS/employees/src/components/employees-add-form/employees-add-form.js
@@ -1,21 +1,48 @@
+
+import { Component } from 'react';
 import './employees-add-form.css'
 
-const EmployeesAddForm = () => {
-  return(
-    <div className="app-add-form">
-      <h3>Додати працівника</h3>
-      <form className="add-form d-flex">
-        <input type="text"
-          className="form-control new-post-label" 
-          placeholder="Його ім'я"/>
-        <input type="number" 
-          className="form-control new-post-label"
-          placeholder="Його з/п у $" />
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: ''
+    }
+  }
 
-          <button type="submit" className="btn btn-outline-light">Додати</button>
-      </form>
-    </div>
-  )
+onValueChange = (e) => {
+  this.setState({
+   [e.target.name]: e.target.value
+  })
+}
+
+  render() {
+
+    const {name, salary} = this.state;
+
+    return(
+      <div className="app-add-form">
+        <h3>Додати працівника</h3>
+        <form className="add-form d-flex">
+          <input type="text"
+            className="form-control new-post-label" 
+            placeholder="Його ім'я" 
+            name="name"
+            value={name}
+            onChange={this.onValueChange}/>
+          <input type="number" 
+            className="form-control new-post-label"
+            placeholder="Його з/п у $" 
+            name="salary"
+            value={salary} 
+            onChange={this.onValueChange}/>
+
+            <button type="submit" className="btn btn-outline-light">Додати</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default EmployeesAddForm;
