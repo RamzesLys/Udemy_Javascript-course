ea3e64e77d2257154d2a8bacbb559c61a9e98fb8
RamzesLys <ramzes311982@gmail.com>
2023-11-15 13:39:59 +0200

    Lesson 135 +

--- Lesson 126, 128, 129, 133-135 new Project React JS/employees/src/components/app/app.js
+++ Lesson 126, 128, 129, 133-135 new Project React JS/employees/src/components/app/app.js
@@ -22,10 +22,14 @@ class App extends Component {
   
   deleteItem = (id) => {
     this.setState(({data}) => {
-    const index = data.findIndex(elem => elem.id == id);
-    data.splice(index, 1);
+      //const index = data.findIndex(elem => elem.id === id);
+      // const before = data.slice(0, index);
+      // const after = data.slice(index + 1);
+
+      //const newArr = [...before, ...after];
+   
     return{
-      data: data
+      data: data.filter(item => item.id !== id)
     }    
     })
   }
